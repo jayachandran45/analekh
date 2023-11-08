6641553c911f3c3ac210bdd78f3b4b2fd59d7be7
jayachandran45 <jayachandran.k11@gmail.com>
2023-11-08 18:17:00 +0530

    m

---
+++ app.js
@@ -0,0 +1,6 @@
+const inpu1Ele = document.querySelector('.input1')
+const span1Ele = document.querySelector(".s-item-1")
+
+document.querySelector('span').addEventListener('click', function() {
+    document.querySelector('span').classList.add('.span');
+});
